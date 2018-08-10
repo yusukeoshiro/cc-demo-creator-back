c206f682d3d1726c16f16f3aa3a5fd892c35a1e5
Yusuke Oshiro <yoshiro@salesforce.com>
2018-08-10 10:07:16 +0900

    added env api

---
+++ app/controllers/api/v1/misc_controller.rb
@@ -0,0 +1,14 @@
+module Api
+    module V1
+        class MiscController < ApplicationController
+            def env
+                render :json => {
+                    "CLOUDINARY_CLOUD_NAME" => Cloudinary.config.cloud_name,
+                    "CLOUDINARY_UPLOAD_PRESET" => ENV["CLOUDINARY_UPLOAD_PRESET"],
+                    "GOOGLE_IMAGE_API_KEY" => ENV["GOOGLE_IMAGE_API_KEY"],
+                }
+            end
+        end
+
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,10 @@ Rails.application.routes.draw do
 
     namespace 'api' do
         namespace 'v1' do
-            post 'catalog', :to => "catalog#submit"
+            post 'catalog', :to => 'catalog#submit'
+            get  'env',     :to => 'misc#env'
         end
+
     end
 
 end
