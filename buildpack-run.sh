b59bef3c4e337289bb534dac232234ef59583e60
Yusuke Oshiro <yoshiro@salesforce.com>
2018-08-10 11:48:03 +0900

    a

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -4,7 +4,9 @@ mkdir ~/.ssh
 echo "--- RUNNING CUSTOM BUILD SCRIPT!!"
 
 echo "--- installing ssh private key"
-eval `ssh-agent` & ssh-keyscan github.com >> ~/.ssh/known_hosts & ssh-add - <<< "${GITHUB_PRIVATE_KEY}"
+eval `ssh-agent`
+sleep 10s
+ssh-keyscan github.com >> ~/.ssh/known_hosts & ssh-add - <<< "${GITHUB_PRIVATE_KEY}"
 
 echo "--- cloning build suite private repo!!"
 cd ~/
