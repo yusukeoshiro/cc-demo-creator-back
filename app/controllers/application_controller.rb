b5d76a2eb2cb4efb2ffe075154e2001792bfef9b
Yusuke Oshiro <yoshiro@salesforce.com>
2018-08-20 19:14:55 +0900

    dealt with cors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::API
+    before_action :add_cors_headers
+
+    def add_cors_headers
+        headers['Access-Control-Allow-Origin'] = '*'
+    end
 end
